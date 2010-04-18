e9d44674ca54e5768e0ac90ea7c518567e2d6349
ryuzee <ryuzee@gmail.com>
2010-04-18 01:13:53 +0000

    インストーラーがタコっぽいので削除

--- app/config/routes.php
+++ app/config/routes.php
@@ -26,14 +26,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/views/pages/home.ctp)...
  */
-if (file_exists(TMP.'not_installed.txt'))
-{
-	Router::connect('/:action', array('controller' => 'installer'));    
-} 
-else 
-{
 	Router::connect('/', array('controller' => 'users', 'action' => 'dashboard', 'home'));
-}
 
 
 /**
--- app/views/installer/thanks.ctp
+++
@@ -1,8 +0,0 @@
-<div class="installer view">
-<em><?php echo $message; ?></em>
-
-<br clear="all" />
-
-<p><?php echo $this->Html->link(__('Start PHPMyScrum', true), array('controller' => 'users', 'action' => 'dashboard')); ?></p>
-
-</div>
