db32bc0c51d74acae3321bfc03e4b71f2ba85c54
ryuzee <ryuzee@gmail.com>
2010-05-29 06:15:51 +0900

    do not apply change_username_to...

--- app/config/migrations/map.php
+++ app/config/migrations/map.php
@@ -1,7 +1,7 @@
 <?php
 $map = array(
-	1 => array(
-		'change_username_to_display_name' => 'M4be9d0b79718406d96e96588061edf5f'),
+//	1 => array(
+//		'change_username_to_display_name' => 'M4be9d0b79718406d96e96588061edf5f'),
 	2 => array(
 		'add_change_logs' => 'M4bff3e6d6450492fafe62110061edf5f'),
 );
